94116058ed9b2fbe1a55b1c17d1f0a1497a9ef6a
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-08-29 00:39:36 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const weatherForm = document.getElementById('weatherForm');
-    const loadingIndicator = document.getElementById('loadingIndicator');
-    const searchHistoryList = document.getElementById('searchHistoryList');
+function showLoading() {
+    document.getElementById("loadingIndicator").style.display = "flex";
+}
 
-    weatherForm.addEventListener('submit', function() {
-        showLoading();
-    });
-
-    searchHistoryList.addEventListener('click', function(event) {
-        if (event.target.tagName === 'LI') {
-            const city = event.target.dataset.city;
-            searchCity(city);
-        }
-    });
-
-    function showLoading() {
-        loadingIndicator.style.display = 'flex';
-    }
-
-    function searchCity(city) {
-        document.getElementById('city').value = city;
-        weatherForm.submit();
-    }
-});
+function searchCity(city) {
+    document.getElementById("city").value = city;
+    document.getElementById("weatherForm").submit();
+}
